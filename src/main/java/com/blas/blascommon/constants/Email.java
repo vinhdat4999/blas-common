ba376adc9f20fe02ac52a8dba13c3695847aaad4
DatNguyen <vinhdat4999@gmail.com>
2022-05-30 21:54:29 +0700

    #comment: release v1.4.0

--- src/main/java/com/blas/blascommon/constants/Email.java
+++ src/main/java/com/blas/blascommon/constants/Email.java
@@ -2,9 +2,9 @@ package com.blas.blascommon.constants;
 
 public class Email {
 
-    public static final int SSL_PORT = 465;
-    public static final int TSL_PORT = 587;
     public static final String EMAIL_SENDER_PREFIX = "email-sender";
+
     public static final String PASSWORD_PREFIX = "password";
+    
     public static final String PORT_PREFIX = "port-sender";
 }
