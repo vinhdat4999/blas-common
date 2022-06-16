80258ca98dcf8a6a0a38988df383c13b01870e91
DatNguyen <vinhdat4999@gmail.com>
2022-06-17 00:26:55 +0700

    #comment: release v1.11.0

---
+++ src/main/java/com/blas/blascommon/utils/fileutils/importfile/Csv.java
@@ -0,0 +1,29 @@
+package com.blas.blascommon.utils.fileutils.importfile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Csv {
+
+    private static List<String[]> importFromCsv(String path) {
+        List<String[]> data = new ArrayList<>();
+        Path pathToFile = Paths.get(path);
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(",");
+                data.add(attributes);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
