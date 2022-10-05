1702da17f381fda3ef1d7b6eeead55d97a2a2eb9
Nguyen Vinh Dat <39378896+vinhdat4999@users.noreply.github.com>
2022-10-06 01:28:17 +0700

    Develop (#62)
    * #comment: update version v.1.14.1
    
    * #comment: release v1.15.0 (#61)

--- src/main/java/com/blas/blascommon/utils/fileutils/ImageUtils.java
+++ src/main/java/com/blas/blascommon/utils/fileutils/ImageUtils.java
@@ -14,14 +14,32 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
+import net.coobird.thumbnailator.Thumbnails;
 
 public class ImageUtils {
 
-  public static BufferedImage resizeImage(byte[] image, int targetWidth,
-      int targetHeight) {
+  public static BufferedImage compressImage(String path, float scale) {
+    try {
+      BufferedImage bufferedImage = getBufferImage(path);
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      Thumbnails.of(bufferedImage).outputFormat("JPG")
+          .size(bufferedImage.getWidth(), bufferedImage.getHeight()).outputQuality(scale)
+          .toOutputStream(outputStream);
+      byte[] data = outputStream.toByteArray();
+      ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+      return ImageIO.read(inputStream);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  public static BufferedImage resizeImage(byte[] image, int targetWidth, int targetHeight) {
     try {
       InputStream is = new ByteArrayInputStream(image);
       BufferedImage originalImage = ImageIO.read(is);
@@ -36,11 +54,19 @@ public class ImageUtils {
     }
   }
 
+  public static BufferedImage getBufferImage(String path) {
+    try {
+      return ImageIO.read(new File(path));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
   public static BufferedImage genQrCode(String content) {
     try {
       QRCodeWriter qrCodeWriter = new QRCodeWriter();
-      BitMatrix matrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, QR_WIDTH,
-          QR_HEIGHT);
+      BitMatrix matrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT);
       return MatrixToImageWriter.toBufferedImage(matrix);
     } catch (WriterException e) {
       throw new RuntimeException(e);
---
+++ src/main/java/com/blas/blascommon/utils/fileutils/ResponseUtils.java
@@ -0,0 +1,50 @@
+package com.blas.blascommon.utils.fileutils;
+
+import static com.blas.blascommon.constants.HttpResponseConfiguration.ACCEPT_RANGES;
+import static com.blas.blascommon.constants.HttpResponseConfiguration.BYTES;
+import static com.blas.blascommon.constants.HttpResponseConfiguration.CHUNK_SIZE;
+import static com.blas.blascommon.constants.HttpResponseConfiguration.CONTENT_LENGTH;
+import static com.blas.blascommon.constants.HttpResponseConfiguration.CONTENT_RANGE;
+import static com.blas.blascommon.constants.HttpResponseConfiguration.CONTENT_TYPE;
+import static com.blas.blascommon.constants.HttpResponseConfiguration.VIDEO_CONTENT;
+import static com.blas.blascommon.utils.fileutils.FileUtils.getFileSize;
+import static com.blas.blascommon.utils.fileutils.FileUtils.readBytesRange;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseUtils {
+
+  public static ResponseEntity<byte[]> prepareResponseVideo(final String path,
+      final String fileType, final String range) {
+    long rangeStart = 0;
+    long rangeEnd = CHUNK_SIZE;
+    final long fileSize = getFileSize(path);
+    if (range == null) {
+      return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+          .header(CONTENT_TYPE, VIDEO_CONTENT + fileType).header(ACCEPT_RANGES, BYTES)
+          .header(CONTENT_LENGTH, String.valueOf(rangeEnd))
+          .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+          .header(CONTENT_LENGTH, String.valueOf(fileSize))
+          .body(readBytesRange(path, rangeStart, rangeEnd));
+    }
+    String[] ranges = range.split("-");
+    rangeStart = Long.parseLong(ranges[0].substring(6));
+    if (ranges.length > 1) {
+      rangeEnd = Long.parseLong(ranges[1]);
+    } else {
+      rangeEnd = rangeStart + CHUNK_SIZE;
+    }
+    rangeEnd = Math.min(rangeEnd, fileSize - 1);
+    final byte[] data = readBytesRange(path, rangeStart, rangeEnd);
+    final String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
+    HttpStatus httpStatus = HttpStatus.PARTIAL_CONTENT;
+    if (rangeEnd >= fileSize) {
+      httpStatus = HttpStatus.OK;
+    }
+    return ResponseEntity.status(httpStatus).header(CONTENT_TYPE, VIDEO_CONTENT + fileType)
+        .header(ACCEPT_RANGES, BYTES).header(CONTENT_LENGTH, contentLength)
+        .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+        .body(data);
+  }
+}
