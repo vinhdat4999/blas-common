478c991ec0c2d9f7087a0db41a678ac110860eda
DatNguyen <vinhdat4999@gmail.com>
2023-05-01 08:12:07 +0700

    #comment: v2.9.0

--- src/main/java/com/blas/blascommon/utils/IdUtils.java
+++ src/main/java/com/blas/blascommon/utils/IdUtils.java
@@ -7,7 +7,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class IdUtils {
 
-  private static SecureRandom random = new SecureRandom();
+  private static final SecureRandom random = new SecureRandom();
 
   public static String genUUID() {
     return UUID.randomUUID().toString();
@@ -17,45 +17,46 @@ public class IdUtils {
     int leftLimit = 48; // numeral '0'
     int rightLimit = 122; // letter 'z'
     int targetStringLength = 6;
-    String generatedString = "";
-    generatedString = random.ints(leftLimit, rightLimit + 1)
+    return random.ints(leftLimit, rightLimit + 1)
         .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97) && i != 79 && i != 111)
         .limit(targetStringLength)
         .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-    return generatedString.toUpperCase();
+        .toString()
+        .toUpperCase();
   }
 
   public static String genMixID(int lengthOfId) {
     int leftLimit = 48; // numeral '0'
     int rightLimit = 122; // letter 'z'
     String generatedString = "";
-    generatedString = random.ints(leftLimit, rightLimit + 1)
+    return random.ints(leftLimit, rightLimit + 1)
         .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97) && i != 79 && i != 111)
         .limit(lengthOfId)
         .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-    return generatedString.toUpperCase();
+        .toString()
+        .toUpperCase();
   }
 
   public static String genNumericID() {
     int leftLimit = 48; // numeral '0'
     int rightLimit = 57; // numeral '9'
     int targetStringLength = 6;
-    String generatedString = random.ints(leftLimit, rightLimit + 1).filter(i -> (i <= 57))
+    return random.ints(leftLimit, rightLimit + 1)
+        .filter(i -> (i <= 57))
         .limit(targetStringLength)
         .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-    return generatedString.toUpperCase();
+        .toString()
+        .toUpperCase();
   }
 
   public static String genNumericID(int lengthOfId) {
     int leftLimit = 48; // numeral '0'
     int rightLimit = 57; // numeral '9'
-    String generatedString = random.ints(leftLimit, rightLimit + 1).filter(i -> (i <= 57))
+    return random.ints(leftLimit, rightLimit + 1)
+        .filter(i -> (i <= 57))
         .limit(lengthOfId)
         .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-    return generatedString.toUpperCase();
+        .toString()
+        .toUpperCase();
   }
 }
