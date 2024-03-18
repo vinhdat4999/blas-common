cb546674f6c91d6009a0bbb8dbd81b33b9b69d54
Dat Nguyen <vinhdat4999@gmail.com>
2024-03-18 10:19:40 +0700

    v5.0.0

---
+++ src/main/java/com/blas/blascommon/properties/BlasRequestConfig.java
@@ -0,0 +1,15 @@
+package com.blas.blascommon.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "blas.request-config")
+public class BlasRequestConfig {
+
+  private int httpRequestTimeout;
+}
