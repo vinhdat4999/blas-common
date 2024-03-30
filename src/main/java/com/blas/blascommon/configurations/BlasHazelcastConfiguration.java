6ef67bca75ad666a166ba38732844a8ca53172b8
Dat Nguyen <vinhdat4999@gmail.com>
2024-03-30 10:43:09 +0700

    v5.3.0
    v5.3.0
    
    v5.3.0
    
    v5.3.0
    
    v5.3.0
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.3.0-SNAPSHOT
    
    v5.4.0-SNAPSHOT
    
    v5.4.0-SNAPSHOT
    
    v5.3.0
    
    v5.3.0
    
    v5.3.0

--- src/main/java/com/blas/blascommon/configurations/BlasHazelcastConfiguration.java
+++ src/main/java/com/blas/blascommon/configurations/BlasHazelcastConfiguration.java
@@ -1,23 +1,48 @@
 package com.blas.blascommon.configurations;
 
+import com.blas.blascommon.properties.HazelcastConfiguration;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Slf4j
 @Configuration
 @EnableCaching
+@RequiredArgsConstructor
 public class BlasHazelcastConfiguration implements CachingConfigurer {
 
+  private final HazelcastConfiguration hazelcastConfiguration;
+
   @Bean
-  public Config hazelcastConfig() {
+  @Profile("local")
+  public Config hazelcastLocalConfig() {
     Config config = new Config();
     config.setInstanceName("blasHazelcastInstance");
+    return config;
+  }
+
+  @Bean
+  @Profile("default")
+  public Config hazelcastConfig() {
+    Config config = new Config();
+    config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
+    config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+    String hazelcastService = hazelcastConfiguration.getHazelcastService();
+    String aksNamespace = hazelcastConfiguration.getAksNamespace();
+    config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
+        .setProperty("namespace", aksNamespace)
+        .setProperty("service-name", hazelcastService);
+    log.info("Kubernetes Hazelcast configuration - Namespace: {} - Hazelcast Service name: {}",
+        aksNamespace, hazelcastService);
     return config;
   }
 
