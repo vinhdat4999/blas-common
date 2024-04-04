de528c39085fb762c3f49f5c35f8ddbb800ad7da
Dat Nguyen <vinhdat4999@gmail.com>
2024-04-04 23:30:52 +0700

    v5.3.0

--- src/main/java/com/blas/blascommon/configurations/BlasHazelcastConfiguration.java
+++ src/main/java/com/blas/blascommon/configurations/BlasHazelcastConfiguration.java
@@ -1,25 +1,32 @@
 package com.blas.blascommon.configurations;
 
+import com.blas.blascommon.properties.HazelcastConfiguration;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
+@RequiredArgsConstructor
 public class BlasHazelcastConfiguration implements CachingConfigurer {
 
-//  @Bean
-//  public Config hazelcastConfig() {
-//    Config config = new Config();
-//    config.setInstanceName("blasHazelcastInstance");
-//    return config;
-//  }
+  private final HazelcastConfiguration hazelcastConfiguration;
+
+  @Bean
+  @Profile("local")
+  public Config hazelcastLocalConfig() {
+    Config config = new Config();
+    config.setInstanceName("blasHazelcastInstance");
+    return config;
+  }
 
   @Bean
   public Config hazelcastConfig() {
@@ -27,7 +34,8 @@ public class BlasHazelcastConfiguration implements CachingConfigurer {
     config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
     config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
     config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
-        .setProperty("namespace", "dev").setProperty("service-name", "hazelcast");
+        .setProperty("namespace", hazelcastConfiguration.getAksNamespace())
+        .setProperty("service-name", hazelcastConfiguration.getHazelcastService());
     return config;
   }
 
